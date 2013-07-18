fc3668fe77b631be39b50c684bc2fa90c2efe9aa
imranshaikh <imran@adshub.net>
2013-07-18 16:09:01 +0530

    Rails intall with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getinvoice::Application.config.secret_token = '90790b154f2ce3d8cef10ac1ac9188ab7d91367a31bd07cd8644cdcb3b2505635a0aa3bba99efc1caa72efaea943da33877bb34b6465a7f3cac8c59effa18d5a'
